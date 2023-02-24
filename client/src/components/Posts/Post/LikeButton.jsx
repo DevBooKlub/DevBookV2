1673840a328df278f7f4c9fbd1bd749fe0b93f7c
Sunny Eyles <sunnyeyles@gmail.com>
2023-02-24 14:03:03 +0100

    Like/unlike button completed

---
+++ client/src/components/Posts/Post/LikeButton.jsx
@@ -0,0 +1,22 @@
+import "./Post.scss";
+import likeImg from "../../../assets/img/like.png";
+import likeImgLight from "../../../assets/img/likeLight.png";
+import redHeartImg from "../../../assets/img/heart(1).png";
+// import
+function LikeButton({ hasLiked, handleLikeUnlike, likesCount, theme }) {
+  return (
+    <div className="item" onClick={handleLikeUnlike}>
+      {hasLiked ? (
+        <img src={redHeartImg} alt="liked image button" />
+      ) : (
+        <img
+          src={theme === "dark" ? likeImgLight : likeImg}
+          alt="like button"
+        />
+      )}
+      <p className="text">{hasLiked ? "Unlike" : "Like"}</p>
+      <span style={{ color: "white", fontSize: "1.25rem" }}>{likesCount}</span>
+    </div>
+  );
+}
+export default LikeButton;
--- client/src/components/Posts/Post/Post.jsx
+++ client/src/components/Posts/Post/Post.jsx
@@ -1,26 +1,27 @@
-import React, { useContext, useState } from 'react'
-import './Post.scss'
-import likeImg from '../../../assets/img/like.png'
-import likeImgLight from '../../../assets/img/likeLight.png'
-import commentImg from '../../../assets/img/comment.png'
-import commentImgLight from '../../../assets/img/commentLight.png'
-import shareImg from '../../../assets/img/share.png'
-import shareImgLight from '../../../assets/img/shareLight.png'
-import Comments from '../../Comments/Comments'
-import { AuthContext } from '../../../context/authContext'
-import defaultUserPic from '../../../assets/img/pepeUserPic.jpg'
-import addFriendLight from '../../../assets/img/addContact.png'
-import addFriendDark from '../../../assets/img/addConntactLight.png'
-import removeFriendDark from '../../../assets/img/removeContactLight.png'
-import removeFriendLight from '../../../assets/img/removeContact.png'
-import deletePostLight from '../../../assets/img/trashLight.png'
-import deletePostDark from '../../../assets/img/trashDark.png'
-import veryfiedIcon from '../../../assets/img/verified.png'
+import React, { useContext, useState } from "react";
+import "./Post.scss";
+import likeImg from "../../../assets/img/like.png";
+import likeImgLight from "../../../assets/img/likeLight.png";
+import commentImg from "../../../assets/img/comment.png";
+import commentImgLight from "../../../assets/img/commentLight.png";
+import shareImg from "../../../assets/img/share.png";
+import shareImgLight from "../../../assets/img/shareLight.png";
+import Comments from "../../Comments/Comments";
+import { AuthContext } from "../../../context/authContext";
+import defaultUserPic from "../../../assets/img/pepeUserPic.jpg";
+import addFriendLight from "../../../assets/img/addContact.png";
+import addFriendDark from "../../../assets/img/addConntactLight.png";
+import removeFriendDark from "../../../assets/img/removeContactLight.png";
+import removeFriendLight from "../../../assets/img/removeContact.png";
+import deletePostLight from "../../../assets/img/trashLight.png";
+import deletePostDark from "../../../assets/img/trashDark.png";
+import veryfiedIcon from "../../../assets/img/verified.png";
+import LikeButton from "./LikeButton";
 
-import moment from 'moment'
+import moment from "moment";
 
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Post({
   theme,
@@ -37,82 +38,82 @@ function Post({
   setPosts,
   numberOfLikes,
 }) {
-  const { user, setUser } = useContext(AuthContext)
-  const navigate = useNavigate()
+  const { user, setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
   const [hasLiked, setHasLiked] = useState(() =>
     likes.find((e) => e._id === user._id) ? true : false
-  )
-  const [likesCount, setLikesCount] = useState(numberOfLikes)
+  );
+  const [likesCount, setLikesCount] = useState(numberOfLikes);
   const handleClick = () => {
-    navigate(`/profile/${postAuthor._id}`)
-  }
+    navigate(`/profile/${postAuthor._id}`);
+  };
 
   const isMyFriend = () =>
-    user.friends.find((friend) => friend._id === postAuthor._id)
+    user.friends.find((friend) => friend._id === postAuthor._id);
 
   const handleAddRemoveFriend = async () => {
     const { data } = await axios({
-      method: 'patch',
+      method: "patch",
       url: `/api/users/${postAuthor._id}`,
-    })
-    setUser(data.data)
-  }
+    });
+    setUser(data.data);
+  };
   const handleDelete = async () => {
     try {
       const res = await axios(`/api/posts/${postID}`, {
-        method: 'DELETE',
+        method: "DELETE",
         withCredentials: true,
-      })
-      if (res.status === 204) setPosts(posts.filter((e) => e._id !== postID))
+      });
+      if (res.status === 204) setPosts(posts.filter((e) => e._id !== postID));
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
+  };
 
   const handleLikeUnlike = async (evt) => {
     try {
       const { data } = await axios(`/api/posts/${postID}`, {
-        method: 'PATCH',
+        method: "PATCH",
         withCredentials: true,
-      })
-      setHasLiked(data.state)
-      setLikesCount(data.data.numberOfLikes)
+      });
+      setHasLiked(data.state);
+      setLikesCount(data.data.numberOfLikes);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
-  }
+  };
 
-  const [commentOpen, setCommentOpen] = useState(false)
+  const [commentOpen, setCommentOpen] = useState(false);
 
-  const userPicURL = `${__URL_BASE__}${postAuthor.userPic}`
-  const postPicURL = `${__URL_BASE__}${image}`
+  const userPicURL = `${__URL_BASE__}${postAuthor.userPic}`;
+  const postPicURL = `${__URL_BASE__}${image}`;
 
   return (
-    <div className='single-post-container backgroundInner box-shadow'>
-      <div className='single-post-wraper'>
-        <div className='user'>
-          <div className='userInfo'>
+    <div className="single-post-container backgroundInner box-shadow">
+      <div className="single-post-wraper">
+        <div className="user">
+          <div className="userInfo">
             {postAuthor.userPic && (
               <img
-                className='user-img borderImg'
+                className="user-img borderImg"
                 onClick={handleClick}
                 src={postAuthor.userPic ? userPicURL : defaultUserPic}
-                alt=''
+                alt=""
               />
             )}
-            <div className='details'>
+            <div className="details">
               {postAuthor.nickname && (
-                <div className='nickname-container'>
-                  <span className='nickname textPostNickname'>{`${'@'}${
+                <div className="nickname-container">
+                  <span className="nickname textPostNickname">{`${"@"}${
                     postAuthor.nickname
                   }`}</span>
-                  <img src={veryfiedIcon} alt='' />
+                  <img src={veryfiedIcon} alt="" />
                 </div>
               )}
-              <div className='nameDate-container'>
-                <span className='user-name text '>{postAuthor.username}</span>
+              <div className="nameDate-container">
+                <span className="user-name text ">{postAuthor.username}</span>
                 <ul>
-                  <li className='post-date text textPostDate'>
+                  <li className="post-date text textPostDate">
                     {moment(createdAt).fromNow()}
                   </li>
                 </ul>
@@ -121,56 +122,51 @@ function Post({
           </div>
           <div
             onClick={handleAddRemoveFriend}
-            className='addFriend-icon-container '
+            className="addFriend-icon-container "
           >
             {!isMyFriend() ? (
               <img
-                src={theme === 'dark' ? addFriendLight : addFriendDark}
-                alt=''
+                src={theme === "dark" ? addFriendLight : addFriendDark}
+                alt=""
               />
             ) : (
               <img
-                src={theme === 'dark' ? removeFriendLight : removeFriendDark}
-                alt=''
+                src={theme === "dark" ? removeFriendLight : removeFriendDark}
+                alt=""
               />
             )}
           </div>
         </div>
-        <div className='content'>
-          <p className='post-text text'>{desc}</p>
-          <img src={postPicURL} alt='' />
+        <div className="content">
+          <p className="post-text text">{desc}</p>
+          <img src={postPicURL} alt="" />
         </div>
-        <div className='info-icons'>
-          <div className='item' onClick={handleLikeUnlike}>
-            <img src={theme === 'dark' ? likeImgLight : likeImg} alt='' />
-            <p className='text'>{hasLiked ? 'Unlike' : 'Like'}</p>
-            <span style={{ color: 'white', fontSize: '1.25rem' }}>
-              {likesCount}
-            </span>
+        <div className="info-icons">
+          <LikeButton
+            hasLiked={hasLiked}
+            handleLikeUnlike={handleLikeUnlike}
+            likesCount={likesCount}
+            theme={theme}
+          />
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <img src={theme === "dark" ? commentImgLight : commentImg} alt="" />
+            <p className="text">Comment</p>
           </div>
-          <div className='item' onClick={() => setCommentOpen(!commentOpen)}>
-            <img src={theme === 'dark' ? commentImgLight : commentImg} alt='' />
-            <p className='text'>Comment</p>
-          </div>
-          {/* <div className='item'>
-            <img src={theme === 'dark' ? shareImgLight : shareImg} alt='' />
-            <p className='text'>Share</p>
-          </div> */}
           {user._id === postAuthor._id ? (
-            <div className='item' onClick={handleDelete}>
+            <div className="item" onClick={handleDelete}>
               <img
-                className={'visible'}
-                src={theme === 'dark' ? deletePostLight : deletePostDark}
-                alt=''
-              />{' '}
-              <p className='text'>Delete</p>
+                className={"visible"}
+                src={theme === "dark" ? deletePostLight : deletePostDark}
+                alt=""
+              />{" "}
+              <p className="text">Delete</p>
             </div>
           ) : null}
         </div>
         {commentOpen && <Comments />}
       </div>
     </div>
-  )
+  );
 }
 
-export default Post
+export default Post;
